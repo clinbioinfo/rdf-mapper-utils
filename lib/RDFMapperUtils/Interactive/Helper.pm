202caefdebeda848523ea7dfbb4c858a56f9e657
Jaideep Sundaram <sundaram.medimmune@gmail.com>
2017-08-08 17:29:07 -0400

    Fixes implementation of interactive CLI support

--- lib/RDFMapperUtils/Interactive/Helper.pm
+++ lib/RDFMapperUtils/Interactive/Helper.pm
@@ -9,6 +9,8 @@ use Term::ANSIColor;
 
 use RDFMapperUtils::Logger;
 use RDFMapperUtils::Config::Manager;
+use RDFMapperUtils::ConfigFileGenerator;
+use RDFMapperUtils::Manager;
 
 use constant TRUE  => 1;
 use constant FALSE => 0;
@@ -172,47 +174,254 @@ sub _initConfigManager {
 sub run {
 
     my $self = shift;
+    $self->_prompt_user_initial();
+}
+
+##-------------------------------------------------------
+##
+## Generate mapper configuration file methods
+##
+##-------------------------------------------------------
+
+sub _display_overview_of_generate_mapper_config_file_step {
+
+    my $self = shift;
+    
+    print "\n\nHere are the steps\n";
+    print "1. step 1\n";
+    print "2. step 2\n";
+    print "3. step 3\n";
+
+    $self->_generate_mapper_config_file();
+}
+
+
+sub _prompt_user_for_infile {
+
+    my $self = shift;
+
+    print "\nPlease paste the path to the input .csv file\n";
+        
+    my $file = <STDIN>;
+        
+    chomp $file;
+
+    $file =~ s/^\s+//;
+    $file =~ s/^\s+$//;
+
+    if (!-e $file){
+        printBoldRed("file '$file' does not exist");
+    }
+
+    if (!-f $file){
+        printBoldRed("'$file' is not a regular file");
+    }
+
+    return $file;
+}
+
+
+sub _run_generate_mapper_config_file_step {
+
+    my $self = shift;
+    
+    my $infile = $self->getInfile();
+
+    if (!defined($infile)){
+        $infile = $self->_prompt_user_for_infile();
+    }
+
+
+    my $infile_type;
+
+    if ($infile =~ /\.csv$/){
+        $infile_type = 'csv';
+
+    }
+    else {
+        printBoldRed("Currently only support files with extension .csv");
+
+        $self->_prompt_user();
+    }
+
+    my $generator = RDFMapperUtils::ConfigFileGenerator::getInstance(
+        outdir               => $self->getOutdir(),
+        outfile              => $self->getOutfile(),
+        verbose              => $self->getVerbose(),
+        infile               => $infile,
+        infile_type          => $infile_type
+        );
+
+    if (!defined($generator)){
+        $self->{_logger}->logconfess("Could not instantiate RDFMapperUtils::ConfigFileGenerator");
+    }
+        
+    $generator->generateMapperConfigFile();
+
     $self->_prompt_user();
 }
 
 sub _generate_mapper_config_file {
 
     my $self = shift;
-    print "Generate mapper config file\n";
+ 
+    # $self->_displaySectionName("Generate mapper config file");
+
+    my $section_name = 'Generate mapper config file';
+
+    my $options_lookup = {
+        'See overview of this step' => \&_display_overview_of_generate_mapper_config_file_step,
+        'Run this step' => \&_run_generate_mapper_config_file_step
+    };
+
+    $self->{_current_options_lookup} = $options_lookup;
+
+    $self->_prompt_user($options_lookup, $section_name);
+}
+
+
+##-------------------------------------------------------
+##
+## Map CSV file to RDF methods
+##
+##-------------------------------------------------------
+
+sub _display_overview_of_map_csv_file_to_rdf_step {
+
+    my $self = shift;
+    
+    print "\n\nHere are the steps\n";
+    print "1. step 1\n";
+    print "2. step 2\n";
+    print "3. step 3\n";
+
+    $self->_map_csv_file_to_rdf();
+}
+
+
+sub _prompt_user_for_mapper_config_file {
+
+    my $self = shift;
+
+    print "\nPlease paste the path to the input mapper config .ini file\n";
+        
+    my $file = <STDIN>;
+        
+    chomp $file;
+
+    $file =~ s/^\s+//;
+    $file =~ s/^\s+$//;
+
+    if (!-e $file){
+        printBoldRed("file '$file' does not exist");
+    }
+
+    if (!-f $file){
+        printBoldRed("'$file' is not a regular file");
+    }
+
+    return $file;
+}
+
+
+sub _run_map_csv_file_to_rdf_step {
+
+    my $self = shift;
+    
+    my $infile = $self->getInfile();
+
+    if (!defined($infile)){
+        $infile = $self->_prompt_user_for_infile();
+    }
+
+    my $infile_type;
+
+    if ($infile =~ /\.csv$/){
+        $infile_type = 'csv';
+
+    }
+    else {
+        printBoldRed("Currently only support files with extension .csv");
+
+        $self->_prompt_user();
+    }
+
+    my $mapper_config_file = $self->getMapperConfigFile();
+
+    if (!defined($mapper_config_file)){
+        $mapper_config_file = $self->_prompt_user_for_mapper_config_file();
+    }
+
+
+    my $manager = RDFMapperUtils::Manager::getInstance(
+        mapper_config_file   => $mapper_config_file,
+        outdir               => $self->getOutdir(),
+        outfile              => $self->getOutfile(),
+        verbose              => $self->getVerbose(),
+        infile               => $infile,
+        infile_type          => $infile_type,
+        );
+
+    if (!defined($manager)){
+        $self->{_logger}->logconfess("Could not instantiate RDFMapperUtils::Manager");
+    }
+        
+    $manager->generateRDFFile();
+
+    $self->_prompt_user();
 }
 
 sub _map_csv_file_to_rdf {
 
     my $self = shift;
-    print "Map CSV file to RDF\n";
+    
+    my $section_name = 'Map CSV file to RDF';
+
+    my $options_lookup = {
+        'See overview of this step' => \&_display_overview_of_map_csv_file_to_rdf_step,
+        'Run this step' => \&_run_map_csv_file_to_rdf_step
+    };
+
+    $self->{_current_options_lookup} = $options_lookup;
+
+    $self->_prompt_user($options_lookup, $section_name);
 }
 
-sub _say_hello_beautiful {
+
+sub _prompt_user_initial {
 
     my $self = shift;
-    print "Hello beautiful\n";
-}
+    
+    my $section_name = 'Main menu';
 
-sub _say_hello_gorgeous {
+    my $options_lookup = {
+        'Generate mapper configuration INI file' => \&_generate_mapper_config_file,
+        'Map CSV file contents to RDF' => \&_map_csv_file_to_rdf
+    };
 
-    my $self = shift;
-    print "Hello gorgeous\n";
+    $self->{_main_options_lookup} = $options_lookup;
+
+    $self->_prompt_user($options_lookup, $section_name);
 }
 
 sub _prompt_user {
 
     my $self = shift;
-    
-    my $options_lookup = {
-        'Generate mapper configuration INI file' => \&_generate_mapper_config_file,
-        'Map CSV file contents to RDF' => \&_map_csv_file_to_rdf,
-        'Say hello beautiful' => \&_say_hello_beautiful,
-        'Say hello gorgeous' => \&_say_hello_gorgeous
-    };
+    my ($options_lookup, $section_name) = @_;
+
+    if (!defined($options_lookup)){
+        $options_lookup = $self->{_main_options_lookup};
+    }
+
+    if (!defined($section_name)){
+        $section_name = 'Main menu';
+    }
+
+    $self->_displaySectionName($section_name);
 
     my $answer;
 
-    print "What would you like to do, huh?\n";
+    print "\n\nWhat would you like to do, huh?\n";
 
     my $ctr = 0;
     
@@ -232,7 +441,7 @@ sub _prompt_user {
 
     while (1){
 
-        print "\nPlease choose an option [$min-$max/q]";
+        print "\nPlease choose an option [$min-$max/m(ain/q(uit)]";
         
         $answer = <STDIN>;
         
@@ -247,6 +456,11 @@ sub _prompt_user {
             printBoldRed("Okay, bye.");
         
             exit(1);
+        }
+
+        if ($answer eq 'M'){
+        
+            $self->_prompt_user(); 
         }
 
         if (exists $options_ctr_lookup->{$answer}){
@@ -380,6 +594,20 @@ sub printBoldRed {
     my ($msg) = @_;
     print color 'bold red';
     print $msg . "\n";
+    print color 'reset';
+}
+
+sub _displaySectionName {
+
+    my $self = shift;
+    my ($msg) = @_;
+    
+    print color 'bold blue';
+    print "*-------------------------------------------------\n";
+    print "*\n";
+    print "*  " . $msg . "\n";
+    print "*\n";
+    print "*-------------------------------------------------\n";
     print color 'reset';
 }
 
